6fc155f87747dd5ee9c01fe9c8225bece7645f68
DHasan2123 <dderany@ymail.com>
2025-01-14 21:30:19 -0600

    Initial commit

---
+++ week-2/quote_program.py
@@ -0,0 +1,9 @@
+""" 
+    Title: quote_program.py
+    Author: Dua Hasan
+    Date: 14 Januray 2025
+    Description: This file prints a quote and attributes it to the original author.
+"""
+# Using the print function to output the quote and its author
+print('"The only limit to our realization of tomorrow is our doubts of today."')
+print("- Franklin D. Roosevelt")# Attribution to the original author
